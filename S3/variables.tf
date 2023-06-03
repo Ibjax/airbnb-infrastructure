b778646d479ecc254cdd8c039ace8cf14d7b2de3
Ibjax <Ibinabojacks5@gmail.com>
2023-06-02 19:13:54 -0500

    adding airbnb common modules

---
+++ S3/variables.tf
@@ -0,0 +1,6 @@
+variable "bucket_name" {
+    type = string
+    description = " describe your variable"
+    default = "ibinabo24-jjtech"
+          
+}
---
+++ sg/variables.tf
@@ -0,0 +1,5 @@
+variable "port_number" {
+    type = number
+    default = 22
+    description = "Fire wall for traffic"
+}
